d7f326cc5dcf57573f3d6dd8bdf9609c158e62ea
宋祥 <790292520@qq.com>
2018-05-18 16:27:55 +0800

    修改

---
+++ readme.md
@@ -0,0 +1,40 @@
+# laravel-pop-alert
+laravel的flash消息插件有很多，但是不想很喜欢bootstrap的那种提示，所以自己稍微改造了下，加了个别的样式
+
+
+用法：
+```
+composer require bacao/alert
+```  
+然后再app.php添加：
+```
+'providers' => [
+    ...
+    Bacao\Alert\AlertProvider::class
+ ]
+```
+当然也可以添加：
+```
+'aliases' => [
+    ...
+    'Alert'=>Bacao\Alert\Alert::class
+]
+```
+页面公共部分添加：
+```
+@include('bcalert::message')
+```
+最后执行：
+```
+php artisan vendor:publish --provider="Bacao\Alert\AlertProvider"
+```
+demo(四种样式可以选择):
+```
+user Bacao\Alert\Alert;
+Alert::success('成功');
+Alert::error('失败');
+Alert::info('默认');
+Alert::warning('警告');
+```
+最后！enjoy！  
+![image](https://github.com/song0223/laravel-pop-alert/blob/master/%E6%BC%94%E7%A4%BA.gif)
--- src/views/message.blade.php
+++ src/views/message.blade.php
@@ -1,37 +1,15 @@
-@foreach (session('flash_notification', collect())->toArray() as $message)
-    @if ($message['overlay'])
-        @include('flash::modal', [
-            'modalClass' => 'flash-modal',
-            'title'      => $message['title'],
-            'body'       => $message['message']
-        ])
-    @else
-        <div class="alert
-                    alert-{{ $message['level'] }}
-        {{ $message['important'] ? 'alert-important' : '' }}"
-             role="alert"
-        >
-            @if ($message['important'])
-                <button type="button"
-                        class="close"
-                        data-dismiss="alert"
-                        aria-hidden="true"
-                >&times;
-                </button>
-            @endif
-
-            {!! $message['message'] !!}
-        </div>
-    @endif
-@endforeach
 @section('msg-js')
-    @if(session('flash_notification', collect())->toArray())
+    <link href="{{url('pop/dist/spop.min.css')}}" rel="stylesheet"/>
+    <link href="{{url('pop/src/styles.min.css')}}" rel="stylesheet"/>
+    <script src="{{url('pop/src/prism.js')}}"></script>
+    <script src="{{url('pop/dist/spop.min.js')}}"></script>
+    @if(Session::has('flash_notification.message'))
         <script>
             spop({
-                template: '{!! $message['message'] !!}',
-                autoclose: 3000
+                template: '{!! session('flash_notification.message') !!}',
+                autoclose: 3000,
+                style: '{{ session('flash_notification.type') }}'
             });
         </script>
     @endif
 @endsection
-{{ session()->forget('flash_notification') }}
